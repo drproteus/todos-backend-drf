3ea7c56f4a8088af62e2b3ee3d4c842225ca3327
Jake Goritski <jgoritski@gmail.com>
2019-12-01 16:10:19 +0000

    add url to serialized

--- api/serializers.py
+++ api/serializers.py
@@ -7,8 +7,12 @@ class TodoSerializer(serializers.Serializer):
     id = serializers.UUIDField(read_only=True, required=False)
     title = serializers.CharField(max_length=256)
     order = serializers.IntegerField(required=False)
+    url = serializers.SerializerMethodField()
     created_at = serializers.DateTimeField(read_only=True, required=False)
     updated_at = serializers.DateTimeField(read_only=True, required=False)
+
+    def get_url(self, obj):
+        return obj.url
 
     def create(self, validated_data):
         return Todo.objects.create(**validated_data)
