0db610319d872184997e496f3d0d9ed6318df929
Jake Goritski <jgoritski@gmail.com>
2019-12-01 15:50:56 +0000

    fix bad required kwarg on model instead of serializer

--- todos/models.py
+++ todos/models.py
@@ -5,6 +5,6 @@ from django.db import models
 class Todo(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=256)
-    order = models.IntegerField(default=0, required=False)
+    order = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
