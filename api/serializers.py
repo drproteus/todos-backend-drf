55ddbd0db893e439af43c6885c580727b309fa1e
Jake Goritski <jgoritski@gmail.com>
2019-12-01 16:19:08 +0000

    include completed field in serialized data

--- api/serializers.py
+++ api/serializers.py
@@ -7,6 +7,7 @@ class TodoSerializer(serializers.Serializer):
     id = serializers.UUIDField(read_only=True, required=False)
     title = serializers.CharField(max_length=256)
     order = serializers.IntegerField(required=False)
+    completed = serializers.BooleanField(required=False)
     url = serializers.SerializerMethodField()
     created_at = serializers.DateTimeField(read_only=True, required=False)
     updated_at = serializers.DateTimeField(read_only=True, required=False)
