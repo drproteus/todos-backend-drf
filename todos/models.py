5dce18c541d493513b1735f41b332578d092e798
Jake Goritski <jgoritski@gmail.com>
2019-12-01 16:10:01 +0000

    url property

--- todos/models.py
+++ todos/models.py
@@ -1,10 +1,17 @@
 import uuid
 from django.db import models
+from django.conf import settings
 
 
 class Todo(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=256)
     order = models.IntegerField(default=0)
+    completed = models.BooleanField(default=False)
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    @property
+    def url(self):
+        return "{}/todos/{}/".format(settings.BASE_URL, self.id)
